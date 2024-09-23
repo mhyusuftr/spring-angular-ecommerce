b453999fed571db101f1fbafba7604127b45e9ff
mhysuftr <muhammadyusuftr@gmail.com>
2024-09-23 12:39:53 +0800

    init base project for base project spring boot

---
+++ simple-online-shop/src/test/java/com/tujuhsembilan/simple_online_shop/SimpleOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tujuhsembilan.simple_online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
